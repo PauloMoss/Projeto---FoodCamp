8aeee8ecd19e16cb3e298ee281d4677b0386be4c
Paulo Moss <paulo.moss@poli.ufrj.br>
2021-04-02 17:29:20 -0300

    Conversa Wpp adicionada + fundo embaçado

--- Script/script.js
+++ Script/script.js
@@ -10,20 +10,26 @@ let checkSobremesa;
 let tipoSobremesa;
 let preçoSobremesa;
 
+let valor;
+let nome;
+let endereço;
+
 function selecionar_pratoPrincipal(prato, tipo) {
-   
+
     const selecionado = document.querySelector(".Prato_Principal .selecionado");
+    const icone = document.querySelector(".Prato_Principal .selecionado ion-icon");
     
     if (selecionado !== null) {
         selecionado.classList.remove("selecionado");
+        icone.classList.add("oculto");
     }
-
     checkPrato = document.querySelector(prato);
     checkPrato.classList.add("selecionado");
+    const iconeCheck = document.querySelector(prato + " ion-icon");
+    iconeCheck.classList.remove("oculto");
 
     const Prato = document.querySelector(tipo);
     tipoPrato = Prato.innerHTML
-
     const Preço = document.querySelector(prato + ' .preço');
     preçoPrato = Preço.innerHTML
     conclusao_Pedido()
@@ -32,37 +38,40 @@ function selecionar_pratoPrincipal(prato, tipo) {
 function selecionar_Bebida(bebida, tipo) {
    
     const selecionado = document.querySelector(".Bebidas .selecionado");
-    
+    const icone = document.querySelector(".Bebidas .selecionado ion-icon");
     if (selecionado !== null) {
         selecionado.classList.remove("selecionado");
+        icone.classList.add("oculto");
     }
 
     checkBebida = document.querySelector(bebida);
     checkBebida.classList.add("selecionado");
+    const iconeCheck = document.querySelector(bebida + " ion-icon");
+    iconeCheck.classList.remove("oculto");
 
     const Bebida = document.querySelector(tipo);
     tipoBebida = Bebida.innerHTML
-
     const Preço = document.querySelector(bebida +' .preço');
     preçoBebida = Preço.innerHTML
     conclusao_Pedido()
 }
 
-
 function selecionar_Sobremesa(sobremesa, tipo) {
    
     const selecionado = document.querySelector(".Sobremesas .selecionado");
-    
+    const icone = document.querySelector(".Sobremesas .selecionado ion-icon");
     if (selecionado !== null) {
         selecionado.classList.remove("selecionado");
+        icone.classList.add("oculto");
     }
 
     checkSobremesa = document.querySelector(sobremesa);
     checkSobremesa.classList.add("selecionado");
+    const iconeCheck = document.querySelector(sobremesa + " ion-icon");
+    iconeCheck.classList.remove("oculto");
 
     const Sobremesa = document.querySelector(tipo);
     tipoSobremesa = Sobremesa.innerHTML
-
     const Preço = document.querySelector(sobremesa +' .preço');
     preçoSobremesa = Preço.innerHTML
     conclusao_Pedido()
@@ -70,11 +79,11 @@ function selecionar_Sobremesa(sobremesa, tipo) {
 
 function conclusao_Pedido() {
 
-    const lista1 = checkPrato.classList.contains("selecionado");
-    const lista2 = checkBebida.classList.contains("selecionado");
-    const lista3 = checkSobremesa.classList.contains("selecionado");
+    const cardapio_prato = checkPrato.classList.contains("selecionado");
+    const cardapio_bebida = checkBebida.classList.contains("selecionado");
+    const cardapio_sobremesa = checkSobremesa.classList.contains("selecionado");
 
-    if ( lista3 === true && lista2 === true && lista1 === true) {
+    if ( cardapio_sobremesa === true &&  cardapio_bebida === true && cardapio_prato === true) {
         const aberto = document.querySelector(".conclusao_pedido button");
         aberto.classList.remove("pedido_aberto");
 
@@ -82,16 +91,20 @@ function conclusao_Pedido() {
         fechado.classList.add("pedido_fechado");
         fechado.innerHTML = "Fechar Pedido";
     }
-
 }
 
 function confirmação_de_Pedido() {
     const fechado = document.querySelector(".conclusao_pedido button")
+    nome = prompt("Por favor, informe o seu nome :");
+    endereço = prompt("Para finalizar, nos informe o seu endereço :")
     
-    if (fechado.classList.contains("pedido_fechado")=== true) {
-       
+    if (fechado.classList.contains("pedido_fechado") === true) {
         const confirmaçao = document.querySelector(".confirme_pedido");
         confirmaçao.classList.remove("oculto");
+        const embassado = document.querySelector('.fundoEmbassado');
+        embassado.classList.remove("oculto");
+        const estatico = document.querySelector('body');
+        estatico.classList.add("estatico");
         
         const Pedido1 = document.querySelector('.pedido1 span');
         Pedido1.innerHTML = tipoPrato;
@@ -109,14 +122,29 @@ function confirmação_de_Pedido() {
         preçoPedido3.innerHTML = 'R$ ' + preçoSobremesa;
 
         const preçoTotal = document.querySelector('.total span:nth-of-type(2)');
-        const valor = Number(preçoPrato) + Number(preçoBebida) + Number(preçoSobremesa);
+        valor = Number(preçoPrato) + Number(preçoBebida) + Number(preçoSobremesa);
         preçoTotal.innerHTML = 'R$ ' + valor.toFixed(2);
     }
-
-}   
+} 
 
 function cancelar_Pedido() {
-   
-        const confirmaçao = document.querySelector(".confirme_pedido");
-        confirmaçao.classList.add("oculto");
+    const confirmaçao = document.querySelector(".confirme_pedido");
+    confirmaçao.classList.add("oculto");
+    const embassado = document.querySelector('.fundoEmbassado');
+    embassado.classList.add("oculto");
+    const estatico = document.querySelector('body');
+    estatico.classList.remove("estatico");
+}
+
+function convera_Wpp() {
+    const Wpp_ola = "Olá, gostaria de fazer o pedido:\n"
+    const Wpp_pedido = " - Prato: " + tipoPrato + "\n - Bebida: " + tipoBebida + "\n - Sobremesa: " + tipoSobremesa;
+    const valor_total = "\nTotal: R$ " + valor.toFixed(2);
+    const dados_cliente = "\n \nNome: " + nome + "\nEndereço: " + endereço;
+    alert(Wpp_ola + Wpp_pedido + valor_total + dados_cliente)
+
+    const msg = encodeURIComponent(Wpp_ola + Wpp_pedido + valor_total + dados_cliente);
+
+    const MudarLink = document.querySelector(".confirme_pedido a");
+    MudarLink.href = MudarLink.href.replace('#', "https://wa.me/?text=" + msg);
 }
